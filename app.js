b18c6d124d6f2e2b00567ce94fdeb70cf624f740
Mahmoud-Altayeb <altayebm804@gmnail.com>
2024-12-17 16:52:39 +0200

    first

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-app.use('/add-product',(req, res, next) => {
-    console.log('In the another middleware');
-    res.send('<h1>test products</h1>');
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/',(req, res, next) => {
-    console.log('In the another middleware');
-    res.send('<h1>test</h1>');
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use('/admin',adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(300);
